fecf675f9b721953f2948ef31fc15c26ed0ca5d1
Felipe Torretto <toreti@gmail.com>
2021-08-08 20:53:41 -0300

    feat: returns the contract owner

--- contracts/SimpleBet.sol
+++ contracts/SimpleBet.sol
@@ -2,4 +2,5 @@
 pragma solidity 0.6.4;
 
 contract SimpleBet {
+    address public owner = msg.sender;
 }
--- test/SimpleBet.test.js
+++ test/SimpleBet.test.js
@@ -1,9 +1,20 @@
 const SimpleBet = artifacts.require("SimpleBet");
 
 contract("SimpleBet", accounts => {
+  // accounts
+  const owner = accounts[0];
+  const user1 = accounts[1];
+
+  // smart contracts
+  let contract;
+
+  before(async () => {
+    contract = await SimpleBet.new();
+  });
+
   describe("Test the contract end to end", async () => {
-    it("Can do anything", async () => {
-      assert.isOk(true, "Something is wrong");
+    it("Anyone can see who is the contract owner", async () => {
+      assert.equal(owner, await contract.owner.call({from: user1}), "The owner is wrong");
     });
   });
 });
