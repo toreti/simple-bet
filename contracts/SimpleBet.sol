38d0d57f67d6d16471fac3a0b997b8d5f9b75af1
Felipe Torretto <toreti@gmail.com>
2021-08-08 21:08:07 -0300

    feat: generate the match hash

--- contracts/SimpleBet.sol
+++ contracts/SimpleBet.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: No License
 pragma solidity 0.6.4;
 
+import './IBEP20.sol';
+
 contract SimpleBet {
     address public owner = msg.sender;
+
+    /**
+     * @notice Generate a unique identifier for a match
+     * @param option1 identifier for a option
+     * @param option2 identifier for other option
+     * @param date of the match, used only to create the hash
+     * @param token allowed to bets
+     * @return a hash used to identify the match
+     */
+    function generateHash(
+        string memory option1,
+        string memory option2,
+        string memory date,
+        IBEP20 token
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(option1, option2, date, token));
+    }
 }
--- test/SimpleBet.test.js
+++ test/SimpleBet.test.js
@@ -1,4 +1,5 @@
 const SimpleBet = artifacts.require("SimpleBet");
+const TestToken = artifacts.require("TestToken");
 
 contract("SimpleBet", accounts => {
   // accounts
@@ -7,14 +8,27 @@ contract("SimpleBet", accounts => {
 
   // smart contracts
   let contract;
+  let token;
 
   before(async () => {
+    token = await TestToken.new();
     contract = await SimpleBet.new();
   });
 
   describe("Test the contract end to end", async () => {
     it("Anyone can see who is the contract owner", async () => {
       assert.equal(owner, await contract.owner.call({from: user1}), "The owner is wrong");
+    });
+    it("Anyone can generate hash to identify a match", async () => {
+      const generateHash = async from => {
+        assert.equal(
+          '0x17921acf9a8e601d2c9e87e4166b32dbe09600281f8faab2053e7eb9d7807350',
+          await contract.generateHash.call("BRA", "USA", "2021-01-30", token.address, {from}),
+          "The hash is wrong"
+        );
+      };
+      await generateHash(owner);
+      await generateHash(user1);
     });
   });
 });
